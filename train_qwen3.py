04f217c6535768e5f54de616dd417a205aaef964
daishuangliang <daisl1@aeroht.com>
2025-08-08 18:07:11 +0800

    init

--- train_qwen3.py
+++ train_qwen3.py
@@ -37,7 +37,10 @@ def dataset_jsonl_transfer(origin_path, new_path):
             # 解析每一行的json数据
             data = json.loads(line)
             input = data["question"]
-            output = f"<think>{data["think"]}</think> \n {data["answer"]}"
+            think = data["think"]
+            ancher = data["ancher"]
+
+            output = f"<think>{think}</think> \n {ancher}"
             message = {
                 "instruction": PROMPT,
                 "input": f"{input}",
